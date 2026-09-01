719f14558523962f57351117212141ed7536e17a
yang967967-rgb <yang967967@gmail.com>
2026-09-01 12:27:34 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,15 @@
-const CACHE_NAME = "shiguang-ledger-v1";
-const APP_FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE_NAME = "shiguang-ledger-v2";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon.svg",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
